a8110188fcb1bcbc71d92a6156dae81b8902ca8d
Sofía Bobbiesi <sofiabobbiesi@gmail.com>
2022-11-02 20:08:13 -0300

    finished ejercicio2, missing comments

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,12 +1,38 @@
 import pandas as pd
+from datetime import datetime, timedelta
+from time import sleep
+
 # Ejercicio 2.1
-_PRODUCT_DF = pd.DataFrame({"product_name": ["Chocolate", "Granizado",
-"Limon", "Dulce de Leche"], "quantity": [3,10,0,5]})
+_PRODUCT_DF = pd.DataFrame(
+    {"product_name": ["Chocolate", "Granizado", "Limon", "Dulce de Leche"],
+ "quantity": [3,10,0,5]})
+
 def is_product_available(product_name, quantity):
-    pass
+    try:
+        product = _PRODUCT_DF[_PRODUCT_DF["product_name"] == product_name]
+        return product["quantity"].values[0] >= quantity
+    except Exception as e:
+        print(e)
+        return False
 
+#  Ejercicio 2.2:
+_RETRIES_ = {}
 
-""" Ejercicio 2.2:
-Si miramos el diagrama de flujo al momento de la decisión de stock, encontramos un
-potencial loop infinito, ya que el usuario podría continuar ingresando productos inválidos
-o sin stock. Reformule la función para solucionar este problema. """
+def is_product_available_2(product_name, quantity):
+    
+    if product_name + "_" + str(quantity) in _RETRIES_:
+        product_time = _RETRIES_[product_name + "_" + str(quantity)]
+        if product_time + timedelta(minutes=5) > datetime.now():
+            print("No se puede realizar la operación, intente más tarde")
+            sleep(300)
+
+    try:
+        product = _PRODUCT_DF[_PRODUCT_DF["product_name"] == product_name]
+        stock = product["quantity"].values[0] >= quantity
+        return stock
+    except:
+        stock = False
+        return False
+    finally:
+        if not stock:
+            _RETRIES_[product_name + "_" + str(quantity)] = datetime.now()
